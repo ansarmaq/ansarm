3c99f73c5713d10f5190cf9536ec46c01162fbec
Ansar Maqsood <P14152540@GWHXX584LIN020W.lec-admin.dmu.ac.uk>
2016-01-28 15:28:37 +0000

    Modified hello world program

--- helloWorld.c
+++ helloWorld.c
@@ -1,14 +1,3 @@
-#include<stdio.h> 
-main()
-{    
-   printf("Hello World\n");
-}
- 
-
-Save, add and commit helloWorld.c with an appropriate message.
-
-Now modify the program so it is reads
-
 #include<stdio.h>  
 main() 
 {     
